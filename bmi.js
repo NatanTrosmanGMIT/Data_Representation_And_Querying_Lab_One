5068a81c95f313c73746f9c803836ca4b3a386b1
NatanTrosmanGMIT <61838651+NatanTrosmanGMIT@users.noreply.github.com>
2021-09-27 14:38:32 +0100

    Add files via upload

--- bmi.js
+++ bmi.js
@@ -1,15 +1,18 @@
+// Create BIM class
 class BMI {
-
+    // Create constructor for BMI
     constructor(height, weight) {
         this.height = height;
         this.weight = weight;
     }
+    // Calculate BMI
     calculateBMI() {
         let bmi = this.weight / (this.height ** 2);
         return bmi;
     }
 }
 
+// Instansiate and display BMI
 let myBMI = new BMI(1.93, 111);
 let calc = myBMI.calculateBMI();
 console.log(calc);
--- cars.js
+++ cars.js
@@ -1,10 +1,12 @@
+// Create Vehicle class
 class Vehicle {
-
+    // Create Vehicle constructor
     constructor(make, model, year) {
         this.make = make;
         this.model = model;
         this.year = year;
     }
+    // Display info method
     Information() {
         console.log(`Make : ${this.make} `);
         console.log(`Model : ${this.model}`);
@@ -12,18 +14,20 @@ class Vehicle {
 
     }
 }
-
+// Create Cars class that extends Vehicle
 class Cars extends Vehicle{
     constructor(make, model, year,doors) {
         super(make, model, year);
         this.doors = doors;
     }
-
+    // Display more info method
     moreInformation() {
         super.Information();
         console.log(`Doors : ${this.doors}`);
     }
 }
+
+// Instansiate and display Vehicles
 
 //let vehicle1 = new Vehicle('Honda','CR-V', 1996);
 let vehicle2 = new Cars('Honda','CR-V', 1996, 5);
